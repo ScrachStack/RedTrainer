8a1a98e22f164b2ba939ac7c77a8a7f842d14d8b
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-26 13:05:32 -0700

    Update oufitcustomization.lua

--- client/oufitcustomization.lua
+++ client/oufitcustomization.lua
@@ -3,13 +3,16 @@ lib.registerMenu({
     title = 'Oufit Customization',
     position = 'top-right',
     options = {
-        {label = 'Male', icon = 'user-tie', args = {id = 'spawn_model'}},
-        {label = 'Female', icon = 'person-dress', args = {id = 'Revolver'}},
+        {label = 'Male', icon = 'user-tie', args = {id = 'male'}},
+        {label = 'Female', icon = 'person-dress', args = {id = 'female'}},
      
 
     }
 
         
 }, function(selected, scrollIndex, args)
-   
+   if args.id == 'male' then
+        else 
+            
+        end
 end)

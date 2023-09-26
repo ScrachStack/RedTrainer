461219a3cff9465198ff34d2ebad20986158b05d
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-26 15:01:44 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,49 @@
+Config = {}
+
+Config.Ace = 'redm'
+Config.MenuCommand = 'menu'
+Config.EnablePermissions = false
+
+Config.ModularSetup = {
+    {
+            MaxHealthCore = true, -- Enable/Disable Max Health Core
+            MaxStaminaCore = true, -- Enable/Disable Max Stamina Core
+            GodMode = true, -- Enable/Disable God Mode
+            ClearPedTasks = true, -- Enable/Disable Clear Ped Tasks
+            Noclip = true, -- Enable/Disable Noclip
+        ["giveweapon"] = { 
+            enabled = true, -- Set to true to enable, false to disable
+            WeaponsMenu = {
+                {
+                    label = "Revolver",
+                    model = "weapon_revolver", -- Replace with the actual model name
+                },
+                {
+                    label = "Bow",
+                    model = "weapon_bow", -- Replace with the actual model name
+                },
+                {
+                    label = "Lasso",
+                    model = "weapon_lasso", -- Replace with the actual model name
+                },
+                {
+                    label = "Molotov",
+                    model = "WEAPON_THROWN_MOLOTOV", -- Replace with the actual model name
+                },
+                {
+                    label = "Navy Revolver",
+                    model = "WEAPON_REVOLVER_NAVY", -- Replace with the actual model name
+                },
+                {
+                    label = "Improved",
+                    model = "WEAPON_BOW_IMPROVED", -- Replace with the actual model name
+                },
+                {
+                    label = "Bolas",
+                    model = "WEAPON_THROWN_BOLAS", -- Replace with the actual model name
+                },
+        },
+    },
+    },
+}
+

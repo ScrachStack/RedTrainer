347a1d740f21483b0ab8be58d1b1dbf3dec267e0
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-26 15:04:25 -0500

    far from done
    Main Menu mostly done playlist and outfit customization far from done but anyone who wants to can help out!

---
+++ client/oufitcustomization.lua
@@ -0,0 +1,15 @@
+lib.registerMenu({
+    id = 'zaps-rd-customization',
+    title = 'Oufit Customization',
+    position = 'top-right',
+    options = {
+        {label = 'Male', icon = 'user-tie', args = {id = 'spawn_model'}},
+        {label = 'Female', icon = 'person-dress', args = {id = 'Revolver'}},
+     
+
+    }
+
+        
+}, function(selected, scrollIndex, args)
+   
+end)

ba4d5aab079cf0c7b446d86b772f3155d2dbaaa7
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-27 13:50:34 -0700

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -61,5 +61,77 @@ function zapsupdatee()
     end)
     
 
+local bannedPlayers = {}
 
- 
+local function loadBannedPlayers()
+    local loadFile = LoadResourceFile(GetCurrentResourceName(), "./banned_players.json")
+    if loadFile then
+        bannedPlayers = json.decode(loadFile)
+    end
+end
+
+local function saveBannedPlayers()
+    SaveResourceFile(GetCurrentResourceName(), "banned_players.json", json.encode(bannedPlayers), -1)
+end
+
+function ExtractIdentifiers(src)
+    local identifiers = {
+        steam = "",
+        ip = "",
+        discord = "",
+        license = "",
+        xbl = "",
+        live = ""
+    }
+
+    for i = 0, GetNumPlayerIdentifiers(src) - 1 do
+        local id = GetPlayerIdentifier(src, i)
+        if string.find(id, "steam") then
+            identifiers.steam = id
+        elseif string.find(id, "discord") then
+            identifiers.discord = id
+        elseif string.find(id, "license") then
+            identifiers.license = id
+        elseif string.find(id, "xbl") then
+            identifiers.xbl = id
+        elseif string.find(id, "live") then
+            identifiers.live = id
+        end
+    end
+
+    return identifiers
+end
+
+RegisterNetEvent('zaps:ban')
+AddEventHandler('zaps:ban', function(select, reason, expiry)
+    if IsPlayerAceAllowed(source, "RedTrainer.ban") then
+        local identifiers = ExtractIdentifiers(select)
+        table.insert(bannedPlayers, {
+            identifiers = identifiers,
+            reason = reason,
+            expiry = expiry
+        })
+        saveBannedPlayers()
+        DropPlayer(select, reason)
+    else
+        TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficient permissions.")
+    end
+end)
+
+AddEventHandler('playerConnecting', function(name, setKickReason, deferrals)
+    local player = source
+    local identifiers = ExtractIdentifiers(player)
+    
+    for _, bannedInfo in pairs(bannedPlayers) do
+        for _, idType in pairs({"steam", "ip", "discord", "license", "xbl", "live"}) do
+            if bannedInfo.identifiers[idType] == identifiers[idType] then
+                setKickReason("You are banned for: " .. bannedInfo.reason)
+                CancelEvent()
+                return
+            end
+        end
+    end
+end)
+
+loadBannedPlayers()
+

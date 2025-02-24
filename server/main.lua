466039751f8295ea5f63d77b7d2876f684d351f3
ScrachStack <122732007+ScrachStack@users.noreply.github.com>
2025-02-23 21:22:37 -0600

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,58 +1,6 @@
-AddEventHandler('onResourceStart', function(resource)
-    if resource == GetCurrentResourceName() then
-        zapsupdatee()
-        
-if resource == 'RedTrainer' then
 
-else 
-print("^1[ERROR] Please rename your resource to RedTrainer")
-end
-end
-end)
-
-function zapsupdatee()
-    local githubRawUrl = "https://raw.githubusercontent.com/Zaps6000/base/main/api.json"
-    local resourceName = "RedTrainer" 
-    
-    PerformHttpRequest(githubRawUrl, function(statusCode, responseText, headers)
-        if statusCode == 200 then
-            local responseData = json.decode(responseText)
-    
-            if responseData[resourceName] then
-                local remoteVersion = responseData[resourceName].version
-                local description = responseData[resourceName].description
-                local changelog = responseData[resourceName].changelog
-    
-                local manifestVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0)
-    
-                print("Resource: " .. resourceName)
-                print("Manifest Version: " .. manifestVersion)
-                print("Remote Version: " .. remoteVersion)
-                print("Description: " .. description)
-    
-                if manifestVersion ~= remoteVersion then
-                    print("Status: Out of Date (New Version: " .. remoteVersion .. ")")
-                    print("Changelog:")
-                    for _, change in ipairs(changelog) do
-                        print("- " .. change)
-                    end
-                    print("Link to Updates: https://discord.gg/cfxdev")
-                else
-                    print("Status: Up to Date")
-                end
-            else
-                print("Resource name not found in the response.")
-            end
-        else
-            print("HTTP request failed with status code: " .. statusCode)
-        end
-    end, "GET", nil, json.encode({}), {})
-    end
-
-
-
-    RegisterNetEvent('zaps:kick')
-    AddEventHandler('zaps:kick', function(select, reason)
+    RegisterNetEvent('scratch:kick')
+    AddEventHandler('scratch:kick', function(select, reason)
         if IsPlayerAceAllowed(source, "RedTrainer.kick") then
         DropPlayer(select, reason)
         else 
@@ -111,8 +59,8 @@ function ExtractIdentifiers(src)
     return identifiers
 end
 
-RegisterNetEvent('zaps:ban')
-AddEventHandler('zaps:ban', function(select, reason, expiry)
+RegisterNetEvent('scratch:ban')
+AddEventHandler('scratch:ban', function(select, reason, expiry)
     if IsPlayerAceAllowed(source, "RedTrainer.ban") then
         local identifiers = ExtractIdentifiers(select)
         table.insert(bannedPlayers, {
